7cc8f23160e67c2ad2e55fcc00505ed156beb263
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-11-24 23:10:38 +0700

    feat: added models User

---
+++ app/models/user.py
@@ -0,0 +1,17 @@
+import uuid
+from utils.database import Base
+from sqlalchemy import TIMESTAMP, Column, ForeignKey, String, Boolean, text 
+from sqlalchemy.dialects.postgresql import UUID 
+from sqlalchemy.orm import relationship
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid.uuid4)
+    username = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    verified = Column(Boolean, nullable=False, server_default='False')
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
+    
