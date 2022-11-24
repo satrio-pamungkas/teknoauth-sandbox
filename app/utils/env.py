2973530c2b78e53cc8ff825974ba33703309aa0b
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-11-24 22:41:19 +0700

    feat: added configuraiton

---
+++ app/utils/env.py
@@ -0,0 +1,15 @@
+from pydantic import BaseSettings
+
+class EnvSettings(BaseSettings):
+    DB_PORT: int
+    DB_USERNAME: str
+    DB_PASSWORD: str
+    DB_NAME: str
+    DB_HOST: str
+    DB_HOSTNAME: str
+    
+    class Config:
+        env_file = './.env'
+        
+
+env = EnvSettings()
