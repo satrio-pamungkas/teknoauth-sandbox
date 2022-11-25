b4c86ca46a451c400afd157a374dee81ca3c0bde
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-11-25 22:30:03 +0700

    feat: added user schema

---
+++ app/schemas/user.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import uuid 
+from pydantic import BaseModel, EmailStr, constr
+
+class UserSchema(BaseModel):
+    id: uuid.UUID
+    username: str 
+    email: EmailStr
+    
+    class Config:
+        orm_mode = True
+        
+
+class CreateUserSchema(UserSchema):
+    password: constr(min_length=6)
+    verified: bool = False
+    
+class LoginUserSchema(BaseModel):
+    email: EmailStr
+    password: constr(min_length=6)
+
+    
