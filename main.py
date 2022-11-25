cb56814a09921d58d8029401941bebc8671685b8
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-11-25 23:03:46 +0700

    feat: added initialization in main py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.utils.env import env
+from app.routers import auth, user
 
 app = FastAPI()
+
+# Default to all IP
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*']
+)
+
+app.include_router(auth.router, tags=['Auth'], prefix='/api/auth')
+app.include_router(auth.router, tags=['User'], prefix='/api/user')
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
