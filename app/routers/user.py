9cb1d09ede706fe7c3f5658b82e66bef86140e4b
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-11-27 22:39:05 +0700

    feat: added register functionality

--- app/routers/user.py
+++ app/routers/user.py
@@ -3,16 +3,21 @@ from ..models import user as model
 from ..utils.database import get_db
 from fastapi import Depends, HTTPException, status, APIRouter, Response
 from sqlalchemy.orm import session
+from passlib.hash import argon2
 import uuid 
 
 router = APIRouter()
 
 @router.post('/register', status_code=status.HTTP_201_CREATED, response_model=schema.UserSchema)
-def create_user(user: schema.CreateUserSchema, db: session = Depends(get_db)):
-    user.id = uuid.uuid4()
-    new_user = model.User(**user.dict())
-    db.add(new_user)
+def create_user(
+    request: schema.CreateUserSchema, 
+    db: session = Depends(get_db)
+):
+    request.id = uuid.uuid4()
+    request.password = argon2.hash(request.password)
+    user = model.User(**request.dict())
+    db.add(user)
     db.commit()
-    db.refresh(new_user)
+    db.refresh(user)
     
-    return new_user
+    return user
