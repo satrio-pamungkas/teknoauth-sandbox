0b4d50cf670fe800f5d81dca53dbdd402040612c
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-11-27 22:54:15 +0700

    feat: added login functionality

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,7 +3,7 @@ from ..models import user as model
 from ..utils.database import get_db
 from fastapi import Depends, HTTPException, status, APIRouter, Response
 from sqlalchemy.orm import Session
-import uuid 
+from passlib.hash import argon2
 
 router = APIRouter()
 
@@ -14,7 +14,7 @@ def login(request: schema.LoginUserSchema, db: Session = Depends(get_db)):
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User is not found')
     
-    if user.password != request.password:
+    if not argon2.verify(request.password, user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Password not match')
     
     return Response(status_code=status.HTTP_200_OK)
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,7 +1,7 @@
 from ..schemas import user as schema
 from ..models import user as model
 from ..utils.database import get_db
-from fastapi import Depends, HTTPException, status, APIRouter, Response
+from fastapi import Depends, status, APIRouter
 from sqlalchemy.orm import session
 from passlib.hash import argon2
 import uuid 
@@ -9,10 +9,7 @@ import uuid
 router = APIRouter()
 
 @router.post('/register', status_code=status.HTTP_201_CREATED, response_model=schema.UserSchema)
-def create_user(
-    request: schema.CreateUserSchema, 
-    db: session = Depends(get_db)
-):
+def create_user(request: schema.CreateUserSchema, db: session = Depends(get_db)):
     request.id = uuid.uuid4()
     request.password = argon2.hash(request.password)
     user = model.User(**request.dict())
